2d296d5b6e77a4156710807031ee55f1c6612c83
Hirunidiv <hirunidivyanjalee365@gmail.com>
2025-03-01 22:31:45 +0530

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require('express');
 import express from 'express';
 import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
@@ -8,8 +9,9 @@ const app = express();
 
 app.get("/products",(req,res) => {});
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
